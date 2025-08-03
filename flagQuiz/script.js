7e7e98615b7204f53d5e68b1ca6aeb90a90f7f54
Michael <johansyahmichael@gmail.com>
2025-08-03 21:14:33 +0700

    pause

---
+++ flagQuiz/script.js
@@ -0,0 +1,24 @@
+const countries = [
+    { name: "Indonesia", flag: "flagImages/Flag_of_Indonesia.svg" },
+    { name: "Argentina", flag: "flagImages/Flag_of_Argentina.svg" },
+    { name: "Australia", flag: "flagImages/Flag_of_Australia.svg" },
+    { name: "Brazil", flag: "flagImages/Flag_of_Brazil.svg" },
+    { name: "England", flag: "flagImages/Flag_of_England.svg" },
+    { name: "India", flag: "flagImages/Flag_of_India.svg" },
+    { name: "Japan", flag: "flagImages/Flag_of_Japan.svg" },
+    { name: "Malaysia", flag: "flagImages/Flag_of_Malaysia.svg" },
+    { name: "Netherlands", flag: "flagImages/Flag_of_Netherlands.svg" },
+    { name: "Palestine", flag: "flagImages/Flag_of_Palestine.svg" },
+    { name: "Republic of China", flag: "flagImages/Flag_of_Peoples_Republic_of_China.svg" },
+    { name: "Portugal", flag: "flagImages/Flag_of_Portugal.svg" },
+    { name: "Singapore", flag: "flagImages/Flag_of_Singapore.svg" },
+    { name: "South_Korea", flag: "flagImages/Flag_of_South_Korea.svg" },
+    { name: "Spain", flag: "flagImages/Flag_of_Spain.svg" },
+    { name: "Thailand", flag: "flagImages/Flag_of_Thailand.svg" },
+    { name: "Turkey", flag: "flagImages/Flag_of_Turkey.svg" },
+    { name: "United_States", flag: "flagImages/Flag_of_United_States.svg" },
+    { name: "Vietnam", flag: "flagImages/Flag_of_Vietnam.svg" },
+];
+
+let currentQuestion = 0;
+let score = 0;
